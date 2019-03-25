1e7104f2356dad189a3925bbbd06b1da1933a82b
Simran <simran_palia@yahoo.co.in>
2019-03-25 17:13:45 +0800

    Comments added for reference.

--- DynamicSampleApp/Program.cs
+++ DynamicSampleApp/Program.cs
@@ -24,22 +24,20 @@ namespace DynamicSampleApp
             d = new { StudentId = 123, StudentName = "Simran" };
 
             Student student = new Student();
-            student.StudentId = d.StudentId;
-            student.StudentName = d.StudentName;
+            student.StudentId = d.StudentId;        //No parsing and run time binding
+            student.StudentName = d.StudentName;    //No parsing and run time binding
 
             //Object
             Object objStudent = new { StudentId = 123, StudentName = "Simran" };
             Student studentNew = new Student();
-            studentNew.StudentId = Convert.ToInt64(objStudent.GetType().GetProperty("StudentId").GetValue(objStudent).ToString());
-            studentNew.StudentName = objStudent.GetType().GetProperty("StudentName").GetValue(objStudent).ToString();
+            studentNew.StudentId = Convert.ToInt64(objStudent.GetType().GetProperty("StudentId").GetValue(objStudent).ToString());      //Explicit parsing using Reflection
+            studentNew.StudentName = objStudent.GetType().GetProperty("StudentName").GetValue(objStudent).ToString();                   //Explicit parsing using Reflection
 
             //Var keyword
             var varObj = new { StudentId = 123, StudentName = "Simran" };
             Student studentVar = new Student();
-            studentVar.StudentId = varObj.StudentId;//Evaluate properties at compile time
-            studentVar.StudentName = varObj.StudentName;//Evaluate properties at compile time
-
-
+            studentVar.StudentId = varObj.StudentId;        //Evaluate properties at compile time with intellisense
+            studentVar.StudentName = varObj.StudentName;    //Evaluate properties at compile time with intellisense
 
             Console.ReadLine();
         }
